78bc56612bb4a73e4099aac7e5e6e9cfe5b2e9bc
EvgenyMechanik <evg-mechanik@yandex.ru>
2020-04-24 01:47:42 +0300

    Add some tests

--- tests/c/fractal.c
+++ tests/c/fractal.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
-void draw_ruler_r(Display *d, Window* w, int s, int xl, int xr, int y, int h)
+void draw_ruler_r(Display *d, Window* w, int s, float xl, float xr, float y, int h)
 {
-	int xm;
+	float xm;
 	if(h) {
 		xm = (xl + xr) / 2;
 		draw_ruler_r(d, w, s, xl, xm, y, h / 2);
@@ -14,15 +15,16 @@ void draw_ruler_r(Display *d, Window* w, int s, int xl, int xr, int y, int h)
 	}
 }
 
-void draw_ruler_v(Display *d, Window* w, int s, int xl, int xr, int y, int h)
+void draw_ruler_v(Display *d, Window* w, int s, float xl, float xr, float y, int h)
 {
-	int i, j, t , delta  = (xr -xl) / (2 * h);	
+	int i, j, t;
+	float delta  = (xr -xl) / (2 * h);	
 	for(i = 1, t = 1; t <= h; t *= 2, i *= 2)
 		for(j = xl + i * delta; j < xr; j += 2 * i * delta)
 			XDrawLine(d, *w, DefaultGC(d, s), j, y, j, y + t);	
 }
 
-void draw_star_r(Display* d, Window* w, int s, int x, int y, int l)
+void draw_star_r(Display* d, Window* w, int s, float x, float y, int l)
 {
 	if(l) {
 		XFillRectangle(d, *w, DefaultGC(d, s), x - l / 2, y - l / 2, l, l);
@@ -33,14 +35,29 @@ void draw_star_r(Display* d, Window* w, int s, int x, int y, int l)
 	}
 }
 
-void draw_kox_star_r(Display* d, Window* w, int s, int x1, int y1, int x2, int y2, int i, int p)
+static void calculate_x_y(float x1, float y1, float ox, float oy, float* x2, float* y2, float a)
 {
-	if(i <= p) {
-		draw_kox_star_r(d, w, s, x1, y1, (2 * x1 + x2) / 3, (2 * y1 + y2) / 3, i + 1, p);
-		draw_kox_star_r(d, w, s, (2 * x1 + x2) / 3, (2 * y1 + y2) / 3,/**/, i + 1, p);
-		draw_kox_star_r(d, w, s,/**/, (x1 + 2* x2) / 3, (y1 + 2 * y2) / 3, i + 1, p);
-		draw_kox_star_r(d, w, s, (x1 + 2* x2) / 3, (y1 + 2 * y2) / 3, x2, y2, i + 1, p);
+	x1 -= ox; 
+	y1 -= oy;
+	*x2 = x1 * cos(a) - y1 * sin(a) + ox;
+	*y2 = x1 * sin(a) + y1 * cos(a) + oy;
+ }
 
+void draw_kox_star_r(Display* d, Window* w, int s, float x1, float y1, float x2, float y2, int i, int p)
+{
+	float xx, yy;
+	if(i <= p) {
+		calculate_x_y(x1, y1, (2 * x1 + x2) / 3, (2 * y1 + y2) / 3, &xx, &yy, 2 * 3.14159 / 3);
+		draw_kox_star_r(d, w, s, x1, y1, (2 * x1 + x2) / 3, (2 * y1 + y2) / 3, i + 1, p);
+		draw_kox_star_r(d, w, s, (2 * x1 + x2) / 3, (2 * y1 + y2) / 3, xx, yy, i + 1, p);
+		draw_kox_star_r(d, w, s, xx, yy, (x1 + 2* x2) / 3, (y1 + 2 * y2) / 3, i + 1, p);
+		draw_kox_star_r(d, w, s, (x1 + 2* x2) / 3, (y1 + 2 * y2) / 3, x2, y2, i + 1, p);
+		if(i == p) {
+			XDrawLine(d, *w, DefaultGC(d, s), x1, y1, (2 * x1 + x2) / 3, (2 * y1 + y2) / 3);
+			XDrawLine(d, *w, DefaultGC(d, s), (2 * x1 + x2) / 3, (2 * y1 + y2) / 3, xx, yy);
+			XDrawLine(d, *w, DefaultGC(d, s), xx, yy, (x1 + 2* x2) / 3, (y1 + 2 * y2) / 3);
+			XDrawLine(d, *w, DefaultGC(d, s), (x1 + 2* x2) / 3, (y1 + 2 * y2) / 3, x2, y2);
+		} 
 	}
 }
 
@@ -67,6 +84,7 @@ int main()
 			draw_ruler_r(d, &w, s, 128, 384, 100, 64);
 			draw_ruler_v(d, &w, s, 128, 384, 300, 64);
 			draw_star_r(d, &w, s, 256, 500, 64);
+			draw_kox_star_r(d, &w, s, 128, 700, 384, 700, 1, 4);
 		}
 
 
--- tests/cpp/hanoi.cpp
+++ tests/cpp/hanoi.cpp
@@ -1,19 +1,55 @@
 #include <iostream>
 #include <cstdlib>
 
-#define N 3
+#define N 5
 
-void hanoi(int src, int dst, int help, int n)
+enum { left, right, _last };
+struct hanoi {	
+	int disk;
+	int src;
+	int dst;
+};
+
+static int ipow(int v, int n)
+{
+	int r = 1;
+	for(int i = 0; i < n; i++)
+		r *= v;
+	return r;
+}
+
+void hanoi_r(int src, int dst, int help, int n)
 {
 	if(n) {
-		hanoi(src, help, dst, n - 1);
+		hanoi_r(src, help, dst, n - 1);
 		std::cout << n << ":" << src << "->" << dst << std::endl;
-		hanoi(help, dst, src, n -1); 
+		hanoi_r(help, dst, src, n -1); 
 	}
+}
+
+void hanoi_n_r(int src, int dst, int help, int n)
+{
+	int t_src, i, j, t, s = ipow(2, n), direction = (n % 2 ? right : left);
+	hanoi *arr = new hanoi[s + 1];
+	if(!arr)
+		return;
+	for(t = 1, i = 1; t <= n; t++, i *= 2, direction = (direction + 1) % _last)
+		for(j = i, t_src = src; j < s; t_src = arr[j].dst, j += 2 * i) {
+			arr[j].disk = t;
+			arr[j].src = t_src;
+			arr[j].dst = (direction == right ? (arr[j].src + 1) % (help + 1) : (arr[j].src - 1 >= 0 ? arr[j].src - 1 : help));
+		}
+
+	for(i = 1; i < s; i++)
+		std::cout << arr[i].disk << ":" << arr[i].src << "->" << arr[i].dst << std::endl;
+
+	delete[] arr;
 }
 
 int main()
 {
-	hanoi(0, 1, 2, N);
+	hanoi_r(0, 1, 2, N);
+	std::cout << std::endl << std::endl;
+	hanoi_n_r(0, 1, 2, N);
 	return EXIT_SUCCESS;
 }
